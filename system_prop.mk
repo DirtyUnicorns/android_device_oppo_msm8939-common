d1d3313643dc3377079929cdb7d20d8e03ca3755
Ranjith Narahari <ranjithn@codeaurora.org>
2015-03-19 16:43:45 +0530

    msm8916_64: Enable B Service adj transition by default
    B Service adjs are pushed to adj 1000 based on ageing. This will
    ensure better concurrencies with cached apps as LMK would first
    kill these services under memory pressure
    
    Change-Id: I16d38725edc343a020f09d7de429daef216a02e8

--- system_prop.mk
+++ system_prop.mk
@@ -67,3 +67,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # IO Scheduler
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.io.scheduler=bfq
+
+#Enable B service adj transition by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bservice_enable=true \
+    ro.sys.fw.bservice_limit=5 \
+    ro.sys.fw.bservice_age=5000
